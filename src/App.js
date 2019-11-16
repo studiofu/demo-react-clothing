2b0ee72ee4e88005eeb98701afcdca26a2e3096c
Johnson Fu <johnson.fu@gmail.com>
2019-11-16 22:30:43 +0800

    add colleciton items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 import {Switch,Route} from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div >
 
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
 
     </div>
   );
